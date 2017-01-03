e055663e4889fe5d007f7122fdf1460cebd21863
xczv <jwong.xi@gmail.com>
2017-01-03 04:54:21 -0800

    Finish code for generating parentheses.

--- 022_generateParentheses/src/Solution.java
+++ 022_generateParentheses/src/Solution.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by Glamdring on 12/19/2016.
@@ -14,8 +16,34 @@ import java.util.List;
 
  */
 public class Solution {
-     public List<String> generateParenthesis(int n) {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.generateParenthesis(1));
+        System.out.println(s.generateParenthesis(2));
+        System.out.println(s.generateParenthesis(3));
+    }
+    public List<String> generateParenthesis(int n) {
+        String p = "(";
+        List validParens = new ArrayList<String>();
+        nextParens(validParens,p,n-1,n);
+        return validParens;
+    }
 
+    public void nextParens(List<String> l, String p, int open, int close) {
+        if(open>0) nextParens(l,p+"(",open-1,close);
+        if(close>0) nextParens(l,p+")",open,close-1);
+        if(open==0 && close==0) if(isValid(p)) {
+            l.add(p);
+        }
+    }
 
+    public boolean isValid(String s) {
+        Stack<Character> p = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i)=='(') p.push(')');
+            else if(p.isEmpty() || s.charAt(i) != p.pop()) return false;
+        }
+        if(!p.isEmpty()) return false;
+        else return true;
     }
 }
