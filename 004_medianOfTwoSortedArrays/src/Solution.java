5cb1b61b874af8866a2a4421e2675c21b0950804
xczv <jwong.xi@gmail.com>
2017-01-22 05:30:34 -0800

    Start median of two sorted arrays.

---
+++ 004_medianOfTwoSortedArrays/src/Solution.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Glamdring on 1/21/2017.
+ */
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //odd+odd, take middle value of each, find median
+        //even+even, two middle values of each, average two middle values of those four
+        //even+odd, three values, select median of the three
+
+    }
+}
