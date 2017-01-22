c59470ffcf1987fa0a5ffe871f1dfd94696d766b
xczv <jwong.xi@gmail.com>
2017-01-22 05:30:59 -0800

    Start next permutation.

---
+++ 031_nextPermutation/src/Solution.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Glamdring on 1/21/2017.
+ */
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        for(int i=nums.length-1;i>0;i--) {
+            if(nums[i-1]>nums[i]) { //swap
+
+            }
+        }
+    }
+}
