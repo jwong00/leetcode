43bff1bc1a84a32586131a329467b95f8d66c7df
xczv <jwong.xi@gmail.com>
2017-02-08 01:30:06 -0800

    List rotates correctly in all cases.

--- 061_rotateList/src/Solution.java
+++ 061_rotateList/src/Solution.java
@@ -7,13 +7,13 @@ public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
         ListNode h = new ListNode(1);
-//        h.next = new ListNode(2);
-//        h.next.next = new ListNode(3);
-//        h.next.next.next = new ListNode(4);
-//        h.next.next.next.next = new ListNode(5);
-        h.print();
+        h.next = new ListNode(2);
+        h.next.next = new ListNode(3);
+        h.next.next.next = new ListNode(4);
+        h.next.next.next.next = new ListNode(5);
+//        h.print();
 //        s.rotateRight(h,2).print();
-        s.rotateRight(h,3).print();
+        s.rotateRight(h, 5).print();
     }
     public ListNode rotateRight(ListNode head, int k) {
         if(head==null || k <= 0) return head;
@@ -22,30 +22,22 @@ public class Solution {
         ArrayList<ListNode> list = new ArrayList<>();
         int length = 0, j=k;
         while(cur.next != null) {
-//            if(j>0) j--; //let cur get ahead
-//            else if(kTrailing.next != null) kTrailing = kTrailing.next;
             list.add(cur);
             cur=cur.next;
             length++;
         }
-//        kTrailing=kTrailing.next;
         list.add(cur);
         length++;
-        System.out.println(length);
-        System.out.println(k);
+        if(length==1 || k%length==0) return head;
+        else {
+            kTrailing = list.get(length - 1- k % length);
+            ListNode newHead;
+            newHead = kTrailing.next;
+            cur = list.get(length-1);
+            kTrailing.next = null;
+            cur.next = head;
+            return newHead;
 
-//        System.out.println(kTrailing.val);
-//        System.out.println(cur.val);
-//        System.out.println(list.get(length - 1 - k % length));
-//        System.out.println(list.get(length-1));
-
-        kTrailing = list.get(length - 1- k % length);
-        ListNode newHead = kTrailing.next;
-        cur = list.get(length-1);
-        kTrailing.next = null;
-        cur.next = head;
-
-        return newHead;
-//        return null;
+        }
     }
 }
