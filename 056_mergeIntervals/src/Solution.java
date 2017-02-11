210e22d5c3339ab1b9e528b98b6ddf1ad32e8f85
xczv <jwong.xi@gmail.com>
2017-02-11 01:57:34 -0800

    Catch cases where later intervals overlap with previous ones, but do not merge.

--- 056_mergeIntervals/src/Solution.java
+++ 056_mergeIntervals/src/Solution.java
@@ -8,17 +8,16 @@ public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
         ArrayList h = new ArrayList<Interval>();
-        h.add(new Interval(0,2));
-        h.add(new Interval(7,9));
-        h.add(new Interval(8,11));
-        h.add(new Interval(1,3));
-        h.add(new Interval(3,6));
+        h.add(new Interval(2,3));
+        h.add(new Interval(4,5));
+        h.add(new Interval(6,7));
+        h.add(new Interval(8,9));
+        h.add(new Interval(1,10));
 //        h.add(new Interval());+
         System.out.println(s.merge(h));
 
     }
     public List<Interval> merge(List<Interval> intervals) {
-//        List<Interval> intervalList = new ArrayList<Interval>();
         if(intervals.size()<2) return intervals;
 
         Interval pre = intervals.get(0);
@@ -30,21 +29,24 @@ public class Solution {
          * 3. intervals are disjoint (no merging)
          * */
 
-        int i=0,j=0;
+        int i=0,j=0, k=0;
         while(i <= intervals.size()-1) {
             pre = intervals.get(i);
             j=i+1;
+            k=j;
             while(j<=intervals.size()-1) {
                 cur = intervals.get(j);
                 if (pre.end >= cur.start && cur.end >= pre.start) {
                     pre.start = Math.min(pre.start, cur.start);
                     pre.end = Math.max(pre.end, cur.end);
                     intervals.remove(j);
-                } else j++; //increment only if not removed
+                    k=i;
+                } else  {
+                    j++; //increment only if not removed
+                }
             }
-            i++;
+            i=k;
         }
-
         return intervals;
     }
 }
