b52089756caf0fb67137a210c0ae395df8d0d0ca
xczv <jwong.xi@gmail.com>
2017-04-27 04:59:00 -0700

    Correctly detect islands using recursive bucket-filling approach.

--- 200_numIslands/src/Solution.java
+++ 200_numIslands/src/Solution.java
@@ -6,41 +6,51 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[][] map = {
-                {1,1,1,3},
-                {1,1,0,0},
-                {0,0,0,1},
-                {1,0,0,0}
+        char[][] map = {
+                {'1','1','1','0'},
+                {'1','1','0','0'},
+                {'0','0','0','0'},
+                {'0','0','0','0'}
         };
-        s.numIslands(map);
+        System.out.println(s.numIslands(map));
     }
-    public int numIslands(int[][] grid) {
-        int cols, rows;
+
+    public int numIslands(char[][] grid) {
+        int cols, rows, islands=0;
         if(grid.length<1) return 0;
         else {
             rows=grid.length;
-            cols=grid[0].length;
+            cols=grid[0].length; //assume all rows of equal length
         }
 
         /* iterate over each row left to right
          * then repeat for  each subsequent column */
-//        System.out.println(Arrays.toString(grid[0]));
         for(int i=0;i<rows;i++) {
-//            System.out.println(Arrays.toString(grid[i]));
             for(int j=0;j<cols;j++) {
-                System.out.print(grid[i][j]+" ");
-                scan(grid,i,j);
+                if(scan(grid,i,j)) islands++;
             }
-            System.out.println( );
         }
 
-        return 0;
+        return islands;
     }
 
-    public boolean scan(int[][] grid, int i,int j) {
-        if(grid[i][j]==0) return false;
+    public boolean scan(char[][] grid, int i,int j) {
+        /* Check if at a 0, or out-of-bounds. */
+        if(     i<0 ||
+                j<0 ||
+                i>grid.length-1 ||
+                j>grid[0].length-1) //assume all rows of equal length
+            return false;
+
+        /* If out-of-bounds, treat it as a 0 and just return false. */
+        if(grid[i][j]=='0') return false;
+        /* Else, must be a 1, which is  part of the island. */
         else {
-            grid[i][j]=0;
+            grid[i][j]='0';
+            scan(grid,i-1,j); //above
+            scan(grid,i+1,j); //below
+            scan(grid,i,j-1); //left
+            scan(grid,i,j+1); //right
             return true;
         }
     }
