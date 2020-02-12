e5c0841cf33d47c1057b4b046854870e0b3fe508
Jeremy Wong <wongj@mhusd.org>
2020-02-12 12:46:37 -0800

    Implement more efficient LRU cache. Passes leetcode's test suite in about 12ms without using any particularly exotic data structures.

--- 146_LRUCache/src/LRUCache.java
+++ 146_LRUCache/src/LRUCache.java
@@ -1,19 +1,78 @@
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class LRUCache {
-    int capacity;
-    int countElements;
-    int counter;
     boolean DEBUG = true;
-    HashMap<Integer,Integer> values;
-    HashMap<Integer,Integer> calls;
+    int capacity;
+    HashMap<Integer,Node> map; //maps key to Node
+    DLL dll;
 
+    public void printList() {
+        dll.print();
+    }
+
+    class DLL {
+        Node head,tail;
+        int capacity;
+        int elements;
+
+        public DLL(int _capacity) {
+            this.capacity = _capacity;
+            this.head = this.tail = null;
+        }
+        public int evict() {
+            return remove(head);
+        }
+        public void print() {
+            Node cur = head;
+            while(cur!=null) {
+                System.out.printf("(%d,%d) ",cur.key,cur.value);
+                cur = cur.next;
+            }
+            System.out.printf("\n");
+        }
+        public int remove(Node node) {
+            if(head == tail) {
+                head = null;
+                tail = null;
+            }
+            else if(node == head) {
+                head = head.next;
+                head.prev = null;
+            }
+            else if(node == tail) {
+                tail = tail.prev;
+                tail.next = null;
+            }
+            else {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+            }
+            return node.key;
+        }
+
+        public void moveToEnd(Node node) {
+            if(head == null) { //0 elements
+                head =  node;
+            }
+            else {
+                node.prev = tail;
+                 node.prev.next = node;
+                node.next = null;
+            }
+            tail = node;
+        }
+    }
     class Node {
+        int key;
         int value;
-        Node prev;
-        Node next;
+        Node prev, next;
+
+        public Node(int key,int value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+            this.prev = null;
+        }
     }
 
     public static void main(String args[]) {
@@ -29,75 +88,66 @@ public class LRUCache {
         cache.get(1);       // returns -1 (not found)
         cache.get(3);       // returns 3
         cache.get(4);       // returns 4
+
+        cache = new LRUCache(1);
+        cache.put(2,1);
+        cache.printList();
+        cache.get(2);
+        cache.printList();
+        cache.put(3,2);
+        cache.printList();
+        cache.get(2);
+        cache.printList();
+        cache.get(3);
     }
 
     public LRUCache(int capacity) {
+        if(DEBUG) System.out.printf("LRU cache with capacity %d allocated...\n", capacity) ;
         this.capacity=capacity;
-        countElements=0;
-        counter=0;
-        values = new HashMap();
-        calls = new HashMap();
+        map = new HashMap<>(capacity);
+        dll = new DLL(capacity);
     }
 
     public int get(int key) {
-//        counter++;
-        if(!values.containsKey(key)) return -1;
-        else {
 
-            increment(key);
-            return values.get(key);
+        if(DEBUG) System.out.printf("cache.get(%d) //returns ",key);
+        Node node = map.get(key);
+        if(node == null) {
+            if(DEBUG) System.out.println("-1");
+            return -1;
+        }
+        else {
+            dll.remove(node);
+            dll.moveToEnd(node);
+            if(DEBUG) System.out.println(node.value);
+            return node.value;
         }
     }
 
     public void put(int key, int value) {
-        if(values.containsKey(key)) {
-//            increment(key);
+        Node node = map.get(key);
+
+        if(DEBUG) System.out.printf("cache.put(%d,%d) ",key,value);
+
+        if(node != null) { //update key, doesn't change number of keys
+            //remove key from current location
+//            cur = values.get(key);
+            node.value = value;
+            dll.remove(node);
         }
-        else if(countElements >= capacity) {
-            //evict
-            evict(key);
-        }
-        else if(countElements < capacity) {
-            countElements++;
-        }
+        else {
+            if (map.size() >= capacity) {
+                int r = dll.evict();
+                map.remove(r);
+                if(DEBUG) System.out.printf("// evicts key %d", r);
 
-        //should always put k,v, so it's outside the conditionals
-        if(DEBUG) System.out.println("returns "+key + DEBUG());
-        increment(key);
-        values.put(key,value);
-    }
-
-    private String DEBUG() {
-        return ", calls: "+counter+", elements: "+countElements;
-    }
-
-    private void increment(int key) {
-        counter++;
-        calls.put(key, counter);
-    }
-
-    private void evict(int key) {
-        //get lowest value from calls
-        int min = Integer.MAX_VALUE;
-        int target = 0;
-
-        //Set<Map.Entry<Integer,Integer>> entries = values.entrySet();
-
-        for(Map.Entry<Integer,Integer> entry : calls.entrySet()) {
-            int callCount = entry.getValue();
-            if(callCount < min) {
-                min = callCount;
-                target = entry.getKey();
             }
+            if(DEBUG) System.out.printf("\n");
+            node = new Node(key, value);
+            map.put(key, node);
+
         }
-
-        values.remove(target);
-        calls.remove(target);
-        --counter;
-        if(DEBUG) System.out.println("evicts key "+target+" to insert "+key+DEBUG());
-
-        //iterate over entrySet until K,V s.t. eS(V) == C(V)
-        //remove K
+        dll.moveToEnd(node);
     }
 }
 /**
