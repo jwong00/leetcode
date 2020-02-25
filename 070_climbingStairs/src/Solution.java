37cc4c475db76b8bee05ff4c6762e6f6ea0cb3cb
Jeremy Wong <wongj@mhusd.org>
2020-02-24 16:49:24 -0800

    Efficient, yet naive solution for climbing stairs problem.

---
+++ 070_climbingStairs/src/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        System.out.println(s.climbStairs(3));
+        System.out.println(s.climbStairs(6));
+    }
+
+    public int[] table = new int[1000];
+    public Solution() {
+        table[1]=1;
+        table[2]=2;
+    }
+
+    public int climbStairs(int n) {
+        if(n<=2) return table[n];
+        else if(table[n]==0) table[n]=climbStairs(n-1)+climbStairs(n-2);
+        return table[n];
+    }
+}
