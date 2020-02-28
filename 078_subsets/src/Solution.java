a1c73714eee68d9a968d73b1fde616da28945ce8
Jeremy Wong <wongj@mhusd.org>
2020-02-28 15:43:43 -0800

    Generates the powerset of a given set using bit fields. Uses recursion to generate all bit fields then treats each field as a map to determine the subset. Definitely not the most efficient implementation.

--- 078_subsets/src/Solution.java
+++ 078_subsets/src/Solution.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
 import java.util.List;
 
 /**
@@ -7,29 +9,53 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] j = {1,2,3,4,5,6,7,8};
-//        s.subsets(j);
-        s.nextSet(j,0,1);
-    }
-    public List<List<Integer>> subsets(int[] nums) {
-        ArrayList<ArrayList<Integer>> subsets = new ArrayList<ArrayList<Integer>>();
-        for(int size=0; size<nums.length; size++) {
-            //generate subsets starting with the one-set, ending with full set
-            nextSet(nums, 0, size);
-        }
-        return null;
-    }
-    public void nextSet(int[] set, /*List<Integer> subset,*/ int index, int size) {
-        if(size>1) {
-            for(int i=index; i<set.length-size;i++) {
-                nextSet(set, /*new ArrayList<>(subset),*/ index+1, size-1);
-            }
-        }
-        else { // size<=1
-            for(int i=index; i<set.length; i++) {
-                System.out.println(set[i]);
-            }
-        }
+        int[] j = {1, 2, 3, 4, 5, 6, 7, 8};
+        s.subsets(j);
 
     }
+
+    public class RecursiveBitSet{
+        BitSet bs;
+        ArrayList<BitSet> setRepresentations;
+        public RecursiveBitSet(int r) {
+            bs=new BitSet(r);
+            setRepresentations = new ArrayList<BitSet>();
+        }
+        void fill(int k, int n)
+        {
+            if (k == n)
+            {
+//                System.out.println(bs);
+                setRepresentations.add((BitSet)bs.clone());
+//                setRepresentations.add(bs);
+                return;
+            }
+            bs.set(k, false);
+            fill(k+1, n);
+            bs.set(k, true);
+            fill(k+1, n);
+        }
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        ArrayList<List<Integer>> subsets = new ArrayList<List<Integer>>();
+        RecursiveBitSet rbs = new RecursiveBitSet(nums.length);
+        rbs.fill(0,nums.length);
+
+        ArrayList<Integer> subset;
+        for(BitSet bs : rbs.setRepresentations) {
+            subset = new ArrayList<Integer>();
+            for(int i=0;i<nums.length;i++) {
+                if(bs.get(i)) subset.add(nums[i]);
+            }
+            subsets.add(subset);
+//            System.out.println(bs);
+        }
+
+        for(List l : subsets) {
+            System.out.println(Arrays.deepToString(l.toArray()));
+        }
+
+        return subsets;
+    }
+
 }
