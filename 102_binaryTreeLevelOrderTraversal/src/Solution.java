81091a30717bbbcefe60f083d3f1f625b32096bb
Jeremy Wong <wongj@mhusd.org>
2020-03-05 16:30:28 -0800

    Implemented efficient(?) solution for traversing  a binary tree in level order. Uses a queue to store every node in a level, then dequeues and process the nodes in order when the level is exhausted.

---
+++ 102_binaryTreeLevelOrderTraversal/src/Solution.java
@@ -0,0 +1,57 @@
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        TreeNode k = new TreeNode(3);
+        k.left = new TreeNode(9);
+        k.right = new TreeNode(20);
+        k.right.left = new TreeNode(15);
+        k.right.right = new TreeNode(7);
+        System.out.println(s.levelOrder(k));
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root==null) return new LinkedList<>();
+
+        LinkedList<TreeNode> curLevel = new LinkedList<>();
+        LinkedList<TreeNode> nextLevel = new LinkedList<>();
+        List<List<Integer>> t = new ArrayList<List<Integer>>();
+        curLevel.offer(root);
+
+        TreeNode cur;
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        while(!curLevel.isEmpty()){
+            cur = curLevel.pollFirst();
+
+            if(cur!=null) {
+                temp.add(cur.val);
+                nextLevel.offerLast(cur.left);
+                nextLevel.offerLast(cur.right);
+            }
+            if(curLevel.isEmpty()){
+                if (!temp.isEmpty()) t.add(temp);
+                curLevel = nextLevel;
+                temp = new ArrayList<>();
+                nextLevel = new LinkedList<>();
+            }
+
+        }
+        return t;
+    }
+}
