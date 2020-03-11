158b2b0c0db7417dde3fdff03f80c5b0b1769f4e
Jeremy Wong <wongj@mhusd.org>
2020-03-11 12:09:58 -0700

    Implement simple and efficient recursive solution. Recurses current and parent values to children to use as lower and upper bounds. Validator will return false on the first illegal node.

--- 098_validateBST/src/Solution.java
+++ 098_validateBST/src/Solution.java
@@ -1,3 +1,6 @@
+import com.sun.source.tree.Tree;
+
+import javax.xml.transform.SourceLocator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,20 +16,30 @@ import java.util.List;
 public class Solution {
     public boolean isValidBST(TreeNode root) {
         if(root==null) return true;
-        ArrayList<Integer> inOrderList = new ArrayList<>();
-        parseNode(root,-1);
-        for(int i=1,k=0;i<inOrderList.size();i++,k++) {
-//            System.out.println(inOrderList.get(i));
-            if(inOrderList.get(i)<inOrderList.get(k)) return false;
-        }
-        return true;
+        return parseNode(root,null,null);
     }
 
-    public Boolean parseNode(TreeNode node, int lastValue) {
-        Boolean result=true;
-        if(node.left!=null) result &= parseNode(node.left,node.val);
-        if(node.val<= lastValue) result &= false;
-        if(node.right!=null) result &= parseNode(node.right,node.val);
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        TreeNode node = new TreeNode(5);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(12);
+        node.right.left = new TreeNode(7);
+        node.right.right = new TreeNode(14);
+        node.right.left.left = new TreeNode(6);
+        node.right.left.right = new TreeNode(8);
+        System.out.println(s.isValidBST(node));
+    }
+
+    public Boolean parseNode(TreeNode node, Integer lower, Integer upper) {
+        if(lower != null && node.val <= lower ) return false;
+        if(upper != null && node.val >= upper ) return false;
+        Boolean result = true;
+        if(node.right != null) result &= parseNode(node.right,node.val, upper);
+        if(node.left != null) result &= parseNode(node.left,lower,node.val);
+//        System.out.println(node.val);
         return result;
     }
+
+
 }
