ed2b24218bb5ed711c8e2c5d5f4cf5b5565d2a18
Jeremy Wong <jwong.xi@gmail.com>
2020-07-15 00:45:09 -0700

    Start on cycle detection.

---
+++ 207_courseSchedule/src/Solution.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+
+class Solution {
+    public static void main(String[] args) {
+        int[][] q = {{1,2}};
+        System.out.println(q.length);
+        System.out.println(q[0].length);
+
+        Solution s = new Solution();
+        int[][] p = {{0,1},{3,4}};
+        s.canFinish(6,p);
+
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites.length==0) return true;
+
+        Boolean[][] adjacency = new Boolean[numCourses][numCourses];
+
+        /*
+        * Represents unvisited nodes in the graph. Removal of a node is analogous to
+        * visiting that node.
+        * */
+        HashSet<Integer> unvisited = new HashSet<>();
+
+        for(int i=0; i<prerequisites.length; i++) {
+            adjacency[prerequisites[i][0]][prerequisites[i][1]] = true;
+            unvisited.add(prerequisites[i][1]);
+        }
+        print(adjacency);
+        return false;
+    }
+
+    public void print(Boolean[][] array) {
+        if(array==null) return;
+        for( int i=0; i<array.length; i++ ) {
+            for (int j=0; j<array[0].length; j++) {
+                System.out.printf("%-5b ",array[i][j]);
+            }
+            System.out.printf("\n");
+        }
+    }
+}
