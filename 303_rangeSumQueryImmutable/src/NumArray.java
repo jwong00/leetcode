eb4136807bfa50725e1f03008bc32603859be76b
Jeremy Wong <jwong.xi@gmail.com>
2021-01-29 11:08:10 -0800

    Much more efficient solution. For any intege n_i, precomputes sum of all integers 0..i and stores them. Uses this precomputed array to find the sum by finding the difference between two precomputed sums.

--- 303_rangeSumQueryImmutable/src/NumArray.java
+++ 303_rangeSumQueryImmutable/src/NumArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class NumArray {
     public static void main(String[] args) {
         int[] nums = {-2,0,3,-5,2,-1};
@@ -5,22 +7,33 @@ class NumArray {
 
         System.out.println(numArray.sumRange(0,2));
         System.out.println(numArray.sumRange(2,5));
+
+        int[] nums2 = {1,4,-6};
+        numArray = new NumArray(nums2);
+
+        System.out.println(numArray.sumRange(0,2));
+        System.out.println(numArray.sumRange(1,2));
+        System.out.println(numArray.sumRange(0,1));
     }
 
-    private final int[] nums;
+    private int[] nums;
 
     public NumArray(int[] nums) {
-        this.nums = nums;
+        if(nums.length<1) return;
+        else this.nums = new int[nums.length];
+
+        this.nums[0] = nums[0];
+
+        for(int i=1;i<nums.length;i++) {
+            this.nums[i]=nums[i]+this.nums[i-1];
+        }
+        System.out.println(Arrays.toString(this.nums));
     }
 
     public int sumRange(int i, int j) {
-        int t=0;
-        for(int k=i;k<=j;k++) {
-            t+=nums[k];
-        }
+        if(i<1) return this.nums[j];
 
-        return t;
-
+        return this.nums[j]-this.nums[i-1];
     }
 }
 
