76626a259c496f9985297f44c5f37f9e038fc6ad
Jeremy Wong <jwong.xi@gmail.com>
2021-02-08 23:50:06 -0800

    Efficient non-lazy(?) implementation of a peeking iterator. Referenced the guava implementation and the 100% implementation.

---
+++ Challenges/2021-02/2021-02-08/src/PeekingIterator.java
@@ -0,0 +1,35 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+import java.util.Iterator;
+
+class PeekingIterator implements Iterator<Integer> {
+    Iterator<Integer> iterator;
+    Integer next;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        this.iterator = iterator;
+        if(iterator.hasNext()) next = iterator.next();
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return next;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer result = next;
+        next = null;
+        if(iterator.hasNext()) next = iterator.next();
+        return result;
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+}
