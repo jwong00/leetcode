63b934b5b08941e0c4134de14d86248b17499edd
Jeremy Wong <jwong.xi@gmail.com>
2021-03-29 21:40:08 -0700

    Working solution, but highly inefficient and probably stupid.

--- Challenges/2021-03/2021-03-29/src/Solution.java
+++ Challenges/2021-03/2021-03-29/src/Solution.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +27,10 @@ class Solution {
         TreeNode q = new TreeNode(1);
         q.left = new TreeNode(2);
         q.right = new TreeNode(3);
+        q.right.left = new TreeNode(4);
+        q.right.right = new TreeNode(5);
 
-        s.flipMatchVoyage(q, new int[] {1,3,2});
+        System.out.println(s.flipMatchVoyage(q, new int[] {1,3,5,7,4}));
     }
     public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
         if(root==null) return null;
@@ -40,19 +43,39 @@ class Solution {
         Stack<TreeNode> st = new Stack<>();
         st.push(root);
 
-        TreeNode cur, last=null;
+        TreeNode cur, left, right;
         int count = 0;
+        int size = 0;
         while(!st.isEmpty()) {
             cur = st.pop();
-            //process cur
+//            System.out.println("Proc node "+cur.val);
 
-            //if cur has two children, peek left and see if it matches voyage
-            //if not, flip, and if still not, return -1
-
+            size = st.size();
+            //push the next two
             if(cur.right!=null) st.push(cur.right);
             if(cur.left!=null) st.push(cur.left);
+
+            //next should be in the voyage, if not, flip, pop to old size, push again and check
+            if(!st.isEmpty() && st.peek().val != voyage[count+1]) {
+                flip(cur);
+                while(st.size()>size) st.pop();
+                if(cur.right!=null) st.push(cur.right);
+                if(cur.left!=null) st.push(cur.left);
+
+//                System.out.println("Nextval is " +st.peek().val+" and voyage[count] is "+voyage[count+1] );
+                if(st.peek().val != voyage[count+1]) {
+//                    System.out.println("HERE!");
+                    ArrayList<Integer> t = new ArrayList<>();
+                    t.add(-1);
+                    return t;
+                }
+                else result.add(cur.val);
+
+            }
             count++;
         }
+
+//        System.out.println("RESULT: " +result);
 
         return result;
     }
