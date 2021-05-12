2f8adde5e745ce2a73935583adbb5d543bf90e43
Jeremy Wong <jwong.xi@gmail.com>
2021-05-12 11:29:19 -0700

    Implement dp solution.

--- Challenges/2021-05/2021-05-12/src/NumMatrix.java
+++ Challenges/2021-05/2021-05-12/src/NumMatrix.java
@@ -6,7 +6,6 @@ class NumMatrix {
                         {4, 1, 0, 1, 7},
                         {1, 0, 3, 0, 5}     };
 
-
         NumMatrix nm = new NumMatrix(n);
         System.out.println(nm.sumRegion(2,1,4,3));
         System.out.println(nm.sumRegion(1,1,2,2));
@@ -16,16 +15,36 @@ class NumMatrix {
 
     public NumMatrix(int[][] matrix) {
         this.matrix = matrix;
+        if(this.matrix==null) return;
+
+        int h=matrix.length, w=matrix[0].length;
+        if(h==0 || w==0) return;
+
+
+        for(int i=0;i<h;i++) {
+            int sum = 0;
+            for(int j=0;j<w;j++) {
+                matrix[i][j]+=sum;
+                sum=matrix[i][j];
+            }
+        }
+
+        for(int j=0;j<w;j++) {
+            int sum = 0;
+            for(int i=0;i<h;i++) {
+                matrix[i][j]+=sum;
+                sum=matrix[i][j];
+            }
+        }
+
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
+        int sum = matrix[row2][col2];
 
-        for(int i=row1;i<=row2;i++) {
-            for(int j=col1;j<=col2;j++) {
-                sum+=matrix[i][j];
-            }
-        }
+        if(row1>0) sum -= matrix[row1-1][col2];
+        if(col1>0) sum -= matrix[row2][col1-1];
+        if(row1>0 && col1>0) sum += matrix[row1-1][col1-1];
 
         return sum;
     }
