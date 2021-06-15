918ee2a376a80562994500700260e5f38b1e13cb
Jeremy Wong <jwong.xi@gmail.com>
2021-06-15 16:58:54 -0700

    Start remove comments problem.

--- 722_removeComments/src/Solution.java
+++ 722_removeComments/src/Solution.java
@@ -1,5 +1,17 @@
+import java.util.List;
+
 class Solution {
     public List<String> removeComments(String[] source) {
+        //CASES
+
+        // line comment
+        // start of line, delete whole line
+        // mid of line, delete from cur_char to eol
+
+        // block comment
+        // set cur char to /*
+        // for each line, find */
+        // if not found, delete whole line, if found, remove char until 1 char after */
 
     }
 }
