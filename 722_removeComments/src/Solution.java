0e6ab9c0ec16dc4f1224cc99b50f5637f18745c5
Jeremy Wong <jwong.xi@gmail.com>
2021-06-21 16:02:20 -0700

    Almost correct solution for removing comments. Doesn't properly handle deletion of implicit newline characters from multi-line block comments yet.

--- 722_removeComments/src/Solution.java
+++ 722_removeComments/src/Solution.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.removeComments(new String[] {"/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"});
+        System.out.println(s.removeComments(new String[] {"/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"}));
+        System.out.println(s.removeComments(new String[]{"a/*comment", "line", "more_comment*/b"}));
+
 
     }
     public List<String> removeComments(String[] source) {
-        //CASES
+        List<String> clean = new ArrayList<String>();
 
         boolean IN_BLOCK_COMMENT = false;
         boolean IN_LINE_COMMENT = false;
@@ -19,32 +22,40 @@ class Solution {
             // process line, char-by-char
             for(int cur_char=0;cur_char<line.length();cur_char++) {
 
-
-                if(line.charAt(cur_char) == '/') {
+                if(cur_char<line.length() && line.charAt(cur_char) == '/') {
                     if(line.charAt(cur_char + 1) == '/') IN_LINE_COMMENT = true;
                     if(line.charAt(cur_char + 1) == '*') IN_BLOCK_COMMENT = true;
+
+                    /*
+                        skip second char of comment signifier, so we don't double count
+                        it as part of a block comment close
+                     */
+                    cur_char++;
                 }
 
-                if(line.charAt(cur_char) == '*' && line.charAt(cur_char + 1) == '/') IN_BLOCK_COMMENT = false;
 
-                if(!IN_BLOCK_COMMENT && !IN_LINE_COMMENT) buf.append(line.charAt(cur_char));
+                //exit comment
+                if(cur_char<line.length() && line.charAt(cur_char) == '*' && line.charAt(cur_char + 1) == '/') {
+                    IN_BLOCK_COMMENT = false;
+
+                    cur_char +=2;
+                }
+
+
+                if(cur_char <line.length() && !IN_BLOCK_COMMENT && !IN_LINE_COMMENT) buf.append(line.charAt(cur_char));
             }
 
             //exit LINE COMMENT state after finishing a line
             IN_LINE_COMMENT = false;
 
+            //append line-buffer to clean source file
+            if(buf.length()>0) clean.add(buf.toString());
 
-
-            // line comment
-
-            // start of line, delete whole line
-            // mid of line, delete from cur_char to eol
-
-            // block comment
-            // set cur char to /*
-            // for each line, find */
-            // if not found, delete whole line, if found, remove char until 1 char after */
         }
+
+        return clean;
+
+
 
     }
 }
