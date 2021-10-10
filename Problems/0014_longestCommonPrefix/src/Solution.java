f27113c0ada7da840acc5b7443633d7e9358c6ae
Jeremy Wong <jwong.xi@gmail.com>
2021-10-10 16:29:22 -0700

    Housekeeping.

--- Problems/0014_longestCommonPrefix/src/Solution.java
+++ Problems/0014_longestCommonPrefix/src/Solution.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Solution {
+class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.longestCommonPrefix(new String[]{"leetcode","leet","let"}));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length==0) return "";
+        int length = strs[0].length();
+
+        for(int i=1;i<strs.length;i++) {
+            length = Math.min(length,helper(strs[0],strs[i]));
+        }
+
+        return strs[0].substring(0,length);
+    }
+
+    public int helper(String w1,String w2) {
+        int length = Math.min(w1.length(),w2.length());
+        int common = 0;
+        for(int i=0;i<length;i++) {
+            if(w1.charAt(i)==w2.charAt(i)) common++;
+            else break;
+        }
+        return common;
+    }
 }
